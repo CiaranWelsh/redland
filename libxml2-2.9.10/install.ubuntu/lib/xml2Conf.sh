631d47b4b77ecf3147b6ea0dcad069d131104f39
Ciaran Welsh <cwelsh2@uw.edu>
2020-03-21 16:39:21 +0000

    added source files and built cmake script for building libxml2 on windows and linux

---
+++ libxml2-2.9.10/install.ubuntu/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/mnt/d/redland/libxml2-2.9.10/install.ubuntu/lib"
+XML2_LIBS="-lxml2 -lz -llzma    -lm "
+XML2_INCLUDEDIR="-I/mnt/d/redland/libxml2-2.9.10/install.ubuntu/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
